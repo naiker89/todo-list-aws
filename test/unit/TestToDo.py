7d881d155503590f7bfc8047ea1ef95e61cc8b7f
Rubén López Díez <ruben@rubenlopez.es>
2022-02-06 12:27:54 +0100

    change '

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -127,7 +127,7 @@ class TestDatabaseFunctions(unittest.TestCase):
         from src.todoList import put_item
         from src.todoList import update_item
         from src.todoList import get_item
-        updated_text = 'Aprender más cosas que DevOps y Cloud en la UNIR'
+        updated_text = 'Aprender mas cosas que DevOps y Cloud en la UNIR'
         # Testing file functions
         # Table mock
         responsePut = put_item(self.text, self.dynamodb)
@@ -147,7 +147,7 @@ class TestDatabaseFunctions(unittest.TestCase):
         print ('Start: atest_update_todo_error')
         from src.todoList import put_item
         from src.todoList import update_item
-        updated_text = "Aprender más cosas que DevOps y Cloud en la UNIR"
+        updated_text = "Aprender mas cosas que DevOps y Cloud en la UNIR"
         # Testing file functions
         # Table mock
         responsePut = put_item(self.text, self.dynamodb)
