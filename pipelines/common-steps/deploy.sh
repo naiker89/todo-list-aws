8434d5b16b4b5a451c3bebb87b52c13e19062502
Rubén López Díez <ruben@rubenlopez.es>
2022-02-08 23:46:01 +0100

    Update deploy.sh

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -2,4 +2,4 @@
 
 set -x
 du -hs * | sort -h
-sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --stack-name jenkins --s3-bucket jenkins
+sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --stack-name jenkins --s3-bucket unirjenkins
